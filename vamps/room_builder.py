e81231cdd3255dc75406907c1538dfae506f3848
mfister <mfister@constantcontact.com>
2017-01-19 16:13:05 -0500

    Add room builder

---
+++ vamps/room_builder.py
@@ -0,0 +1,5 @@
+from vamps.room import Room
+
+
+def build_room(level):
+    return Room(0, 0, 60, 32, None, None, None)
--- vamps/state_controller.py
+++ vamps/state_controller.py
@@ -2,16 +2,20 @@ import pyglet
 
 from freezegame.abstract_state import AbstractState
 from vamps.room import Room
+import vamps.room_builder
 
 
 class StateController(AbstractState):
     def __init__(self):
+        self.level = 1
         self.current_state = Room(0, 0, 60, 32, None, None, None)
 
     def update(self, dt, keys):
         self.current_state.update(dt, keys)
         if self.current_state.player_on_open_door:
-            self.current_state = Room(0, 0, 60, 32, None, None, None)
+            next_room = vamps.room_builder.build_room(self.level)
+            self.level += 1
+            self.current_state = next_room
 
     def draw(self):
         self.current_state.draw()
