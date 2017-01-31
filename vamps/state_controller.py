4ccd766052c54ef6970af518fd9442e0b737ebd0
mfister <mfister@constantcontact.com>
2017-01-31 10:12:23 -0500

    Add debug mode and level skip button

--- vamps/state_controller.py
+++ vamps/state_controller.py
@@ -1,6 +1,8 @@
+import pyglet
+
 from freezegame.abstract_state import AbstractState
 import vamps.room_builders.room_factory
-
+import vamps.constants
 
 class StateController(AbstractState):
     def __init__(self):
@@ -9,7 +11,7 @@ class StateController(AbstractState):
 
     def update(self, dt, keys):
         self.current_state.update(dt, keys)
-        if self.current_state.player_on_open_door:
+        if self.current_state.player_on_open_door or (vamps.constants.DEBUG and keys[pyglet.window.key._1]):
             next_room = vamps.room_builders.room_factory.build_room(self.level, self.current_state.player)
             self.level += 1
             self.current_state = next_room
